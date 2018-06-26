814e2b66f8555eee01ac60e8d1b75dbc7a7cf5b9
Seong ho Hong <grohong76@gmail.com>
2018-06-26 23:40:03 +0900

    [feature]: focusbox setting

--- ScrollViewMinimap/ScrollViewMinimap/ScrollViewMinimap.swift
+++ ScrollViewMinimap/ScrollViewMinimap/ScrollViewMinimap.swift
@@ -9,31 +9,41 @@
 import UIKit
 
 class ScrollViewMinimap: UIView {
-    private var focusedBorderWidth: CGFloat = 1.5
+    private var focusedBorderWidth: CGFloat = 2.0
     private var focusedBorderColor: CGColor = UIColor.red.cgColor
-    
+
     private var focusedBoxView: UIView = UIView()
     private var minimapImageView: UIImageView = UIImageView()
-    
+
     private var downSizeRatio: CGFloat = 5.0
-    
+
     public func setUp(scrollView: UIScrollView) {
         // for after set auto layout size
         // todo: not work in viewdidload -> now work in viewdidappear
         scrollView.setNeedsLayout()
         scrollView.layoutIfNeeded()
-        
+
         let width = scrollView.frame.width/downSizeRatio
         let height = scrollView.frame.height/downSizeRatio
-        
+
         // minimap size is divide scollview size into downSizeRatio
         self.frame.size = CGSize(width: width, height: height)
         minimapImageView.frame = CGRect(origin: .zero,
                                         size: CGSize(width: width, height: height))
         
-        minimapImageView.image = scrollView.asImage()
-        self.addSubview(minimapImageView)
-        
+        // focusbox set
+        focusedBoxView.frame = CGRect(origin: .zero,
+                                      size: CGSize(width: width, height: height))
+        focusedBoxView.layer.borderWidth = focusedBorderWidth
+        focusedBoxView.layer.borderColor = focusedBorderColor
+
+        // set minimap image by scrollview first image
+        // todo: queue setting
+        DispatchQueue.main.async {
+            self.minimapImageView.image = scrollView.asImage()
+            self.addSubview(self.minimapImageView)
+            self.addSubview(self.focusedBoxView)
+        }
     }
 }
 
--- ScrollViewMinimap/ViewController.swift
+++ ScrollViewMinimap/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
         scrollView.delegate = self
     }
 
